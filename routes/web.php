00e251c0a9bc6422621066541baceb0976811c09
josua7 <josua7@hotmail.com>
2020-11-30 18:04:15 +0100

    Add Horizon

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\Dummy;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/add', function () {
+    Dummy::dispatch();
+    echo 'Job Dummy dispatched';
 });
